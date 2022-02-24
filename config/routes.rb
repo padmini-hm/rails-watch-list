b0fe164dc5b13ae5e062792b3b2696d637ae88ec
Padmini Hiriyur <padmini.hm@gmail.com>
2022-02-24 16:12:57 +1100

    Added list and bookmark controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get 'lists', to: "lists#index", as: :lists
+  # get 'lists/new', to: "lists#new", as: :list_new
+  # get 'list/:id', to: "lists#show", as: :list
+  # post 'lists', to: "lists#create",
+
+  # get 'lists/bookmarks/new', to: "bookmarks#new", as: :bookmark_new
+  # post 'bookmarks' to: "bookmarks#create",
+  # delete "bookmarks/:id", to: "bookmarks#destroy"
+  # # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :lists, only: [ :index, :new, :create, :show] do
+    resources :bookmarks, only: [ :new, :create ]
+  end
+  resources :bookmarks, only: [ :destroy ]
+
 end
