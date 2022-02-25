dae1cec5bed80b79179e0c68a7e0842bc952f9e8
Padmini Hiriyur <padmini.hm@gmail.com>
2022-02-25 12:54:40 +1100

    updated file config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
   # post 'bookmarks' to: "bookmarks#create",
   # delete "bookmarks/:id", to: "bookmarks#destroy"
   # # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  resources :lists, only: [ :index, :new, :create, :show] do
+  root to "lists#index"
+  resources :lists, only: [:index, :new, :create, :show] do
     resources :bookmarks, only: [ :new, :create ]
   end
   resources :bookmarks, only: [ :destroy ]
